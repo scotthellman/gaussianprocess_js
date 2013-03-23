1c33072baa04fdf4689104046094706e26998d05
scutellaris <scott@192.168.0.4>
2013-03-23 16:59:30 -0500

    started linear tests, converted everything to vectors

--- tests.js
+++ tests.js
@@ -8,26 +8,31 @@
 
 	test( "constant test input invariance ", function(){
 		var constant = Kernels.constant(1);
-		ok(constant.kernel(1,1) == constant.kernel(-5,10));
+		ok(constant.kernel($V([1]),$V([1])) == constant.kernel($V([-5,3]),$V([2,10])));
 	});
 
 	test( "constant test output depends on theta ", function(){
 		var constant = Kernels.constant(10);
-		equal(constant.kernel(3,1),10);
+		equal(constant.kernel($V([3]),$V([1])),10);
 	});
 
 	test( "constant test output handles 0 theta ", function(){
 		var constant = Kernels.constant(0);
-		equal(constant.kernel(3,1),0);
+		equal(constant.kernel($V([3]),$V([1])),0);
 	});
 
 	test( "contant gradient input invariance", function(){
 		var constant = Kernels.constant(1);
-		ok(constant.gradients[0](1,1) == constant.gradients[0](-5,10));
+		ok(constant.gradients[0]($V([1]),$V([1])) == constant.gradients[0]($V([-5,2]),$V([10,20])));
 	});
 
 	test( "contant gradient is 1", function(){
 		var constant = Kernels.constant(1);
-		equal(constant.gradients[0](0,-21),1);
+		equal(constant.gradients[0]($V([0]),$V([-21])),1);
+	});
+
+	test( "linear test output is linear ", function(){
+		var linear = Kernels.linear(1);
+		equal(linear.kernel($V([2]),$V([3])),6);
 	});
 })();
