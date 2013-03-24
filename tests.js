e5c9d350627323498572c64cda996285be995b64
scutellaris <scott@192.168.0.4>
2013-03-24 16:51:38 -0500

    switching to strict equality

--- tests.js
+++ tests.js
@@ -10,32 +10,32 @@
 
 	test( "constant test input invariance ", function(){
 		var constant = Kernels.constant(1);
-		ok(constant.kernel($V([1]),$V([1])) == constant.kernel($V([-5,3]),$V([2,10])));
+		ok(constant.kernel($V([1]),$V([1])) === constant.kernel($V([-5,3]),$V([2,10])));
 	});
 
 	test( "constant test output depends on theta ", function(){
 		var constant = Kernels.constant(10);
-		equal(constant.kernel($V([3]),$V([1])),10);
+		deepEqual(constant.kernel($V([3]),$V([1])),10);
 	});
 
 	test( "constant test output handles 0 theta ", function(){
 		var constant = Kernels.constant(0);
-		equal(constant.kernel($V([3]),$V([1])),0);
+		deepEqual(constant.kernel($V([3]),$V([1])),0);
 	});
 
 	test( "contant gradient input invariance", function(){
 		var constant = Kernels.constant(1);
-		ok(constant.gradients[0]($V([1]),$V([1])) == constant.gradients[0]($V([-5,2]),$V([10,20])));
+		ok(constant.gradients[0]($V([1]),$V([1])) === constant.gradients[0]($V([-5,2]),$V([10,20])));
 	});
 
 	test( "contant gradient is 1", function(){
 		var constant = Kernels.constant(1);
-		equal(constant.gradients[0]($V([0]),$V([-21])),1);
+		deepEqual(constant.gradients[0]($V([0]),$V([-21])),1);
 	});
 
 	test( "linear test output is linear ", function(){
 		var linear = Kernels.linear(1);
-		equal(linear.kernel($V([2]),$V([3])),6);
+		deepEqual(linear.kernel($V([2]),$V([3])),6);
 	});
 
 	test( "linear test output correct in higher dimensions ", function(){
@@ -53,42 +53,47 @@
 
 	test( "linear test output handles 0 theta", function(){
 		var linear = Kernels.linear(0);
-		equal(linear.kernel($V([2,4]),$V([3,-3])), 0);
+		deepEqual(linear.kernel($V([2,4]),$V([3,-3])), 0);
 	});
 
 	test( "linear gradient correctness ", function(){
 		var linear = Kernels.linear(4);
-		equal(linear.gradients[0]($V([2]),$V([3])),6);
+		deepEqual(linear.gradients[0]($V([2]),$V([3])),6);
 	});
 
 	test( "linear gradient multivariate correctness ", function(){
 		var linear = Kernels.linear(-1);
-		equal(linear.gradients[0]($V([-1,2]),$V([0,3])),6);
+		deepEqual(linear.gradients[0]($V([-1,2]),$V([0,3])),6);
 	});
 
 	test( "gaussian is constant on diagonal" , function(){
 		var gauss = Kernels.gaussianNoise(1);
-		equal(gauss.kernel($V([0]),$V([0])), gauss.kernel($V([4]),$V([4])));
+		deepEqual(gauss.kernel($V([0]),$V([0])), gauss.kernel($V([4]),$V([4])));
 	});
 
 	test( "gaussian is constant on diagonal multivariate" , function(){
 		var gauss = Kernels.gaussianNoise(1);
-		equal(gauss.kernel($V([0,4]),$V([0,4])), gauss.kernel($V([20,-2]),$V([20,-2])));
+		deepEqual(gauss.kernel($V([0,4]),$V([0,4])), gauss.kernel($V([20,-2]),$V([20,-2])));
 	});
 
 	test( "gaussian is 0 on off-diagonal" , function(){
 		var gauss = Kernels.gaussianNoise(1);
-		equal(gauss.kernel($V([0]),$V([1])), 0);
+		deepEqual(gauss.kernel($V([0]),$V([1])), 0);
 	});
 
 	test( "gaussian is 0 on off-diagonal multivariate" , function(){
 		var gauss = Kernels.gaussianNoise(1);
-		equal(gauss.kernel($V([0,20]),$V([1,21])), 0);
+		deepEqual(gauss.kernel($V([0,20]),$V([1,21])), 0);
 	});
 
 	test( "gaussian scales with theta" , function(){
 		var gauss = Kernels.gaussianNoise(5);
-		equal(gauss.kernel($V([4,4]),$V([4,4])), 5);
+		deepEqual(gauss.kernel($V([4,4]),$V([4,4])), 5);
+	});
+
+	test( "gaussian has no gradient" , function(){
+		var gauss = Kernels.gaussianNoise(2);
+		deepEqual(gauss.gradients,[]);
 	});
 
 })();
